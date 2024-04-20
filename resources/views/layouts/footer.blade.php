63aabd3b701b8da25ddb597b2047a3a99c605a08
pietro <p.audifreddi.2619@vallauri.edu>
2024-04-20 10:06:11 +0200

    nav migorata

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,7 @@
 
 @if($theme == 'light')
     @vite(['resources/css/footer/footerLight.css'])
-    <h3>ORTO 2.0 © 2024</h3>
 @elseif($theme == 'dark')
     @vite(['resources/css/footer/footerDark.css'])
-    <h3>ORTO 2.0 © 2024</h3>
 @endif
+<h3>ORTO 2.0 © 2024</h3>

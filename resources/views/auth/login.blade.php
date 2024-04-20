90b63bdcde323f33b90aea161c9d75954396494f
pietro <p.audifreddi.2619@vallauri.edu>
2024-04-20 12:18:55 +0200

    cooretto pagina login, non vanno errori

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,41 @@
-<!-- TODO: sistemare grafica -->
-
 @vite(['resources/css/login.css'])
 
 <x-app-layout theme="dark" currentPage="login" pageTitle="Login">
-    <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')"/>
-
-
     <h3 class="floating-Text">
         Hi there, <span class="italic">log in</span><br>and start exploring :)
     </h3>
-
-
     <div id="card">
-        <form method="POST" action="{{ route('login') }}">
+        <h1>My account</h1>
+        <form method="POST" action="{{ route('welcome') }}">
             @csrf
-
-            <!-- Email Address -->
+            <div class="user-Account">
+                <x-text-input id="userEmail" type="email" name="email" :value="old('email')" required autofocus
+                              autocomplete="email" placeholder="EMAIL"/>
+                <x-input-error :messages="$errors->get('email')"/>
+            </div>
             <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <x-text-input id="userPassword" type="password" name="password" required autofocus
+                              placeholder="PASSWORD" autocomplete="current-password"/>
+                <x-input-error :messages="$errors->get('password')"/>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                              type="password"
-                              name="password"
-                              required autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div>
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                    <a id="forgot" href="{{ route('password.request') }}">
+                        {{ __('Forgot?') }}
                     </a>
                 @endif
-
-                <x-primary-button class="ms-3">
-                    {{ __('Log in') }}
+            </div>
+            <div class="google-btn-Space">
+                <a href="{{ route('welcome') }}">
+                    <button type="button" class="login-with-google-btn">
+                        Continue with Google
+                    </button>
+                </a>
+            </div>
+            <div id="validation-Button">
+                <x-primary-button id="enter-Account">
+                    {{ __('JOIN') }}
                 </x-primary-button>
             </div>
         </form>

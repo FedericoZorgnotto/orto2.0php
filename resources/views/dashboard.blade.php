7ea4fe4b42fd7176c6a8f2ac708787fa500d1746
pietro <p.audifreddi.2619@vallauri.edu>
2024-04-22 11:53:05 +0200

    aggiunte immagini e impaginazione ourproduct.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -97,6 +97,9 @@
         }
     }
 </script>
+
+
+
 {{--
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
--- resources/views/ourproduct.blade.php
+++ resources/views/ourproduct.blade.php
@@ -1,5 +1,13 @@
 @vite('resources/css/ourproduct.css')
 
 <x-app-layout theme="dark" currentPage="purchase" pageTitle="Our product">
+    <div id="container">
+        <div class="card">
+            <img src="{{ URL("/videos/showcase1.gif") }}" alt="Orto">
+        </div>
+        <div class="card">
+            <img src="{{ URL("/videos/Untitled.gif") }}" alt="Orto">
+        </div>
+    </div>
 
 </x-app-layout>

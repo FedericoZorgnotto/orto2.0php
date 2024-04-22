37ae52f75cfd243a23270ad17d6de744958e08bc
pietro <p.audifreddi.2619@vallauri.edu>
2024-04-22 12:58:32 +0200

    migliorata dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,16 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-
 <x-app-layout theme="light" currentPage="dashboard" pageTitle="Dashboard">
     <div id="wrapper">
         <div class="card">
-            <select name="tokens" id="tokens">
-            </select>
+            <div id="interaction">
+            <h1>My tokens</h1>
+                <select name="tokens" id="tokens">
+                </select>
+            </div>
             <canvas id="speedChart" width="600" height="400"></canvas>
         </div>
-        <div id="seller-Info">
-
-        </div>
-
     </div>
 </x-app-layout>
 
@@ -97,63 +95,3 @@
         }
     }
 </script>
-
-
-
-{{--
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                <h2>valori temperature sensori</h2>
-            </div>
-            <div class="p-6 text-gray-900">
-                <h2>annunci</h2>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-
-<input type="text" id="postTitle" placeholder="POST TITLE" name="title" value="{{ old('title') }}"/>
-            <div>
-                <input type="text" id="address" placeholder="ADDRES"/>
-                <div class="negative-Feedback hide" id="addressError">
-                    <div class="ball-Negative-Feedback"></div>
-                    Add an address
-                </div>
-            </div>
-            <div>
-                <input type="text" id="quantity" placeholder="QUANTITY"/>
-                <div class="negative-Feedback hide" id="quantityError">
-                    <div class="ball-Negative-Feedback"></div>
-                    Add a quantity
-                </div>
-            </div>
-            <div>
-                <input type="text" id="price" placeholder="PRICE" name="price" value="{{ old('price') }}"/>
-                <div class="negative-Feedback hide" id="priceError">
-                    <div class="ball-Negative-Feedback"></div>
-                    Add a price
-                </div>
-            </div>
-            <div>
-                <textarea id="postContent" rows="4" cols="50" placeholder="DESCRIPTION" name="description">{{ old('description') }}</textarea>
-                <div class="negative-Feedback hide" id="postContentError">
-                    <div class="ball-Negative-Feedback"></div>
-                    Add a Description
-                </div>
-            </div>
-            <button id="create-Post" type="submit">POST</button>
-
-
-<div id="container">
-                <h2>Add some pictures of your product</h2>
-                <input type="file" id="addImage">
-                <img src="" id="previewImage" class="invisible">
-                <button id="add-photos">ADD</button>
-            </div>
---}}
